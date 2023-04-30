774a6ac479b4677c74a50123a16b047506d78a56
al719 <88440404+al719@users.noreply.github.com>
2023-04-30 07:34:31 +0200

    Update fifo_intrface.sv

--- FIFO/fifo_intrface.sv
+++ FIFO/fifo_intrface.sv
@@ -6,5 +6,5 @@ interface FIFO_if (input bit clk);
 	modport DUT (input data_in, wr_en, rd_en, rst_n,clk,output data_out, full, almostfull, empty, almostempty, overflow, underflow, wr_ack);
 	modport TEST (output data_in, wr_en, rd_en, rst_n,clk ,input data_out,data_out_expected, full, almostfull, empty, almostempty, overflow, underflow, wr_ack);
 	modport monitor (input data_in, wr_en, rd_en, rst_n, data_out,data_out_expected, full, almostfull, empty, almostempty, overflow, underflow, wr_ack);
-	modport asert (input clk, wr_en, rd_en, rst_n, full, almostfull, empty, almostempty, overflow, underflow);
+	modport asert (input clk, wr_en, rd_en, rst_n, full, almostfull, empty, almostempty, overflow, underflow,wr_ack);
 endinterface
